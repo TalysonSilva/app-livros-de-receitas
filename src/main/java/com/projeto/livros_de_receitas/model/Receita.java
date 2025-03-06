e15e9961a217e15c30ebec8947feebd6284e8fad
TalysonSilva <talyson.rodigues@gmail.com>
2025-03-05 22:52:21 -0300

    refactor: add novo var para relacionar receita e usuario que cadastrou.

--- src/main/java/com/projeto/livros_de_receitas/model/Receita.java
+++ src/main/java/com/projeto/livros_de_receitas/model/Receita.java
@@ -4,50 +4,52 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 public class Receita {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    private java.lang.String id;
 
-    private String nome;
+    private java.lang.String nome;
 
     @OneToMany(mappedBy = "receita", cascade = CascadeType.ALL, orphanRemoval = true,  fetch = FetchType.EAGER)
     @JsonManagedReference
     private List<Ingrediente> ingredientes;
 
     private int tempoDeCozimento;
-    private int redimento;
+    private int rendimento;
 
     @Enumerated(EnumType.STRING)
     private NivelDificuldade nivelDificuldade;
 
+    private String emailUsuario;
+
     private int estrelas;
 
     @ElementCollection
-    private  List<String> imagens;
+    private  List<java.lang.String> imagens;
 
-    public Receita(String nome, List<Ingrediente> ingredientes, int tempoDeCozimento,
-                   int redimento, NivelDificuldade nivelDificuldade, int estrelas, List<String> imagens) {
+    public Receita(java.lang.String nome, List<Ingrediente> ingredientes, int tempoDeCozimento,
+                   int rendimento, NivelDificuldade nivelDificuldade,
+                   List<java.lang.String> imagens, String emailUsuario) {
         this.nome = nome;
         this.ingredientes = ingredientes;
         this.tempoDeCozimento = tempoDeCozimento;
-        this.redimento = redimento;
+        this.rendimento = rendimento;
         this.nivelDificuldade = nivelDificuldade;
-        this.estrelas = estrelas;
         this.imagens = imagens;
+        this.emailUsuario = emailUsuario;
     }
-     public Receita (){
 
-     }
+    public Receita (){}
 
-    public String getId() {
+
+    public java.lang.String getId() {
         return id;
     }
 
-    public String getNome() {
+    public java.lang.String getNome() {
         return nome;
     }
 
@@ -59,15 +61,12 @@ public class Receita {
         return tempoDeCozimento;
     }
 
-    public int getRedimento() {
-        return redimento;
+    public int getRendimento() {
+        return rendimento;
     }
 
-    public int getEstrelas() {
-        return estrelas;
-    }
 
-    public void setNome(String nome) {
+    public void setNome(java.lang.String nome) {
         this.nome = nome;
     }
 
@@ -79,31 +78,40 @@ public class Receita {
         this.tempoDeCozimento = tempoDeCozimento;
     }
 
-    public void setRedimento(int redimento) {
-        this.redimento = redimento;
+    public void setRendimento(int rendimento) {
+        this.rendimento = rendimento;
+    }
+
+    public List<java.lang.String> getImagens() {
+        return imagens;
+    }
+
+    public void setImagens(List<java.lang.String> imagens) {
+        this.imagens = imagens;
+    }
+
+
+    public void setNivelDificuldade(NivelDificuldade nivelDificuldade) {
+        this.nivelDificuldade = nivelDificuldade;
+    }
+
+    public String getUsuario() {
+        return emailUsuario;
+    }
+
+    public void setUsuario(String string) {
+        this.emailUsuario = string;
+    }
+
+    public NivelDificuldade getNivelDificuldade() {
+        return nivelDificuldade;
+    }
+
+    public int getEstrelas() {
+        return estrelas;
     }
 
     public void setEstrelas(int estrelas) {
         this.estrelas = estrelas;
-    }
-
-    public List<String> getImagens() {
-        return imagens;
-    }
-
-    public void setImagens(List<String> imagens) {
-        this.imagens = imagens;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (!(object instanceof Receita receita)) return false;
-        return tempoDeCozimento == receita.tempoDeCozimento && redimento == receita.redimento && estrelas == receita.estrelas && Objects.equals(nome, receita.nome) && Objects.equals(ingredientes, receita.ingredientes);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(nome, ingredientes, tempoDeCozimento, redimento, estrelas);
     }
 }
