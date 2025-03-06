2d142fe5eee00364afba79a8cb93251383df6821
TalysonSilva <talyson.rodigues@gmail.com>
2025-03-05 22:45:01 -0300

    fix: Organizando classe

--- src/main/java/com/projeto/livros_de_receitas/infra/security/TokenService.java
+++ src/main/java/com/projeto/livros_de_receitas/infra/security/TokenService.java
@@ -1,12 +1,11 @@
 package com.projeto.livros_de_receitas.infra.security;
 
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+
 import com.projeto.livros_de_receitas.model.Usuario;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +17,7 @@ import java.time.ZoneOffset;
 public class TokenService {
 
     @Value("${api.security.token}")
-    private  String secretKey;
+    private String secretKey;
 
     public String gerandoToken (Usuario usuario) {
         try {
